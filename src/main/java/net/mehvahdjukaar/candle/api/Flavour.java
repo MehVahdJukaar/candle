fdab2f8c1241e4d83369b431d3933316eb49d8b4
MehVahdJukaar <matteogozzi1998@gmail.com>
2026-01-01 01:28:00 +0100

    more stuff

--- src/main/java/net/mehvahdjukaar/candle/api/Flavour.java
+++ src/main/java/net/mehvahdjukaar/candle/api/Flavour.java
@@ -5,6 +5,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Flavour { }
